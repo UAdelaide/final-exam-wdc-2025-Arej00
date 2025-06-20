5063c42da5d3e38fd339f042c4fa96a8d502cdad
Arej02 <khadkaarya20@outlook.com>
2025-06-20 04:01:53 +0000

    [automatic commit] 2025-06-20 04:01:53 UTC 115.64.238.242

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -33,7 +33,8 @@ router.get('/api/walkers/summary', async (req, res) => {
     SELECT u.username AS walker_username,
     COUNT(r.rating_id) AS total_ratings,
     ROUND(AVG(r.rating),2) AS average_rating,
-    COUNT(DISTINCT wr_completed)
+    COUNT(DISTINCT wr_completed.request_id) AS completed_walks
+    FROM 
   `);
   res.json(rows);
 });
