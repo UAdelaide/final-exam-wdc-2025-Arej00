f61e41ab15e3938488409f7f292de239d034fffe
Arej02 <khadkaarya20@outlook.com>
2025-06-20 03:53:14 +0000

    [automatic commit] 2025-06-20 03:53:13 UTC 115.64.238.242

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -18,7 +18,7 @@ router.get('/api/dogs', async (req, res) => {
 
 router.get('/api/walkrequests/open', async (req, res) => {
   const [rows] = await db.query(`
-    SELECT w
+    SELECT wr.request_id,d.name AS dog_name,wr.requested_time
   `);
   res.json(rows);
 });
