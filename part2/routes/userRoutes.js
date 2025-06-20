c436813f0c5242cfa10be3aceedfb6528a347154
Arej02 <khadkaarya20@outlook.com>
2025-06-20 06:46:55 +0000

    [automatic commit] 2025-06-20 06:46:55 UTC 115.64.238.242

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -72,26 +72,6 @@ router.post('/login', async (req, res) => {
   }
 });
 
-const requireAuth=(req,res,next) => {
-  if(!req.session.user){
-    return res.status(401).json({error: 'Authentication required' });
-  }
-  next();
-};
 
-const requireRole = (role) => {
-  return (req, res, next) => {
-    if (!req.session.user) {
-      return res.status(401).json({ error: 'Authentication required' });
-    }
-    if (req.session.user.role !== role) {
-      return res.status(403).json({ error: 'Insufficient permissions' });
-    }
-    next();
-  };
-};
-
-router.requireAuth = requireAuth;
-router.requireRole = requireRole;
 
 module.exports = router;
