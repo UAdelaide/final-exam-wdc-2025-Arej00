51c43a02083cb73470ff9fb0c9c942b8235d0652
Arej02 <khadkaarya20@outlook.com>
2025-06-20 04:03:20 +0000

    [automatic commit] 2025-06-20 04:03:20 UTC 115.64.238.242

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -37,7 +37,8 @@ router.get('/api/walkers/summary', async (req, res) => {
     FROM Users u
     LEFT JOIN WalkRatings r ON u.user_id=r.walker_id
     LEFT JOIN WalkRequests wr_completed
-    ON r.request_id=wr_completed.request_id AND wr
+    ON r.request_id=wr_completed.request_id AND wr_completed_status='completed'
+    WHERE 
   `);
   res.json(rows);
 });
