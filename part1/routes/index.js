bccfc8f5a259d01582b505505a49c013245f77b1
Arej02 <khadkaarya20@outlook.com>
2025-06-20 03:31:40 +0000

    [automatic commit] 2025-06-20 03:31:40 UTC 115.64.238.242

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -6,9 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-app.get('/api/dogs',async(req,res)=>{
-  
-})
-
+router.get('/api/dogs', async (req, res) => {
+  const [rows] = await db.query(`
+    SELECT bl.BookID, bi.Title, u.Name AS SellerName, bl.SellerID
+    FROM BookListings bl
+    JOIN BookInfo bi ON bl.BookInfoID = bi.BookInfoID
+    JOIN Users u ON bl.SellerID = u.UserID
+  `);
+  res.json(rows);
+});
 
 module.exports = router;
