5c5a3b9323cab223036f5875bdc3864225c7cf0d
Arej02 <khadkaarya20@outlook.com>
2025-06-20 03:54:41 +0000

    [automatic commit] 2025-06-20 03:54:41 UTC 115.64.238.242

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -22,6 +22,7 @@ router.get('/api/walkrequests/open', async (req, res) => {
     wr.duration_minutes,wr.location,u.username AS owner_username
     FROM WalkRequests wr
     JOIN Dogs d ON wr.dog_id=d.dog_id
+    JOIN Users u ON d.owner_id=u.user
   `);
   res.json(rows);
 });
