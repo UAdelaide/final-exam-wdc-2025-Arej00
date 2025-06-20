ae28c4c3b02740e52a359f41e520100427f5ad5f
Arej02 <khadkaarya20@outlook.com>
2025-06-20 08:05:56 +0000

    [automatic commit] 2025-06-20 08:05:56 UTC 115.64.238.242

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -90,7 +90,7 @@ router.get('/api/users/me/dogs', async (req, res) => {
   }
   try {
     const [rows] = await db.query(`
-      SELECT dog;
+      SELECT dog_id, name FROM Dogs WHERE owner_id=?`,;
 
     res.status(201).json({ message: 'User registered', user_id: result.insertId });
   } catch (error) {
