83130a5eccfb07694d4e7783202debe08220ab72
Arej02 <khadkaarya20@outlook.com>
2025-06-20 06:43:11 +0000

    [automatic commit] 2025-06-20 06:43:11 UTC 115.64.238.242

--- part2/app.js
+++ part2/app.js
@@ -20,7 +20,14 @@ app.get('/owner-dashboard.html',(req,res) => {
         return res.redirect('/');
     }
     res.sendFile(path.join(__dirname,'public','owner-dashboard.html'));
-})
+});
+
+app.get('/walker-dashboard.html',(req,res) => {
+    if(!req.session.user || req.session.user.role !=='owner'){
+        return res.redirect('/');
+    }
+    res.sendFile(path.join(__dirname,'public','owner-dashboard.html'));
+});
 
 // Export the app instead of listening here
 module.exports = app;
