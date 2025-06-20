1625ba8e517d1632cf3ce40c3e1b98351779c69c
Arej02 <khadkaarya20@outlook.com>
2025-06-20 03:57:11 +0000

    [automatic commit] 2025-06-20 03:57:11 UTC 115.64.238.242

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -30,12 +30,8 @@ router.get('/api/walkrequests/open', async (req, res) => {
 
 router.get('/api/walkers/summary', async (req, res) => {
   const [rows] = await db.query(`
-    SELECT wr.request_id,d.name AS dog_name,wr.requested_time,
-    wr.duration_minutes,wr.location,u.username AS owner_username
-    FROM WalkRequests wr
-    JOIN Dogs d ON wr.dog_id=d.dog_id
-    JOIN Users u ON d.owner_id=u.user_id
-    WHERE wr.status='open'
+    SELECT u.username AS walker_username,
+    COUNT
   `);
   res.json(rows);
 });
