e4a954dbd6dabfba184fffadd3d946e05c40a8a3
Arej02 <khadkaarya20@outlook.com>
2025-06-21 02:56:01 +0000

    [automatic commit] 2025-06-21 02:56:01 UTC 115.64.238.242

--- part2/app.js
+++ part2/app.js
@@ -14,11 +14,11 @@ app.use(session({
     cookie: { secure: false }
 }));
 
-app.use(express.static(path.join(__dirname, '/public')));
-
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
